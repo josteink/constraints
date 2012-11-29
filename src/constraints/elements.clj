42b4312a7e6ae6d249966d53db5362672bc57d9b
Jostein Kjønigsen <jostein@kjonigsen.net>
2012-11-29 09:42:13 +0100

    Tidied up. Added tests.

--- src/constraints/elements.clj
+++ src/constraints/elements.clj
@@ -4,73 +4,75 @@
 ; implementations
 
 (defn adder [a1 a2 sum]
-	(let [me (atom nil)
-		  dispatcher {
-			:type "Adder"
-			:i-have-a-value (fn []
-				(cond (and (has-value? a1) (has-value? a2))
-					    (set-value! sum (+ (get-value a1) (get-value a2)) @me)
-					  (and (has-value? a1) (has-value? sum))
-					    (set-value! a2 (- (get-value sum) (get-value a1)) @me)
-					  (and (has-value? a2) (has-value? sum))
-					    (set-value! a1 (- (get-value sum) (get-value a2)) @me)))
-			:i-lost-my-value (fn []
-				(forget-value! sum @me)
-				(forget-value! a1 @me)
-				(forget-value! a2 @me))
-		}]
-		(reset! me dispatcher)
-		(connect a1 @me)
-		(connect a2 @me)
-		(connect sum @me)
-		dispatcher))
+  (let [me (atom nil)
+        dispatcher {
+                    :type "Adder"
+                    :i-have-a-value
+                    (fn []
+                      (let [[[hv1 hv2 hvs][v1 v2 vs]] (dmap [has-value? get-value] [a1 a2 sum])]
+                        (cond
+                          (and hv1 hv2) (set-value! sum (+ v1 v2) @me)
+                          (and hv1 hvs) (set-value! a2 (- vs v1) @me)
+                          (and hv2 hvs) (set-value! a1 (- vs v2) @me))))
+                    :i-lost-my-value
+                    (fn []
+                      (forget-value! sum @me)
+                      (forget-value! a1 @me)
+                      (forget-value! a2 @me))
+                    }]
+    (reset! me dispatcher)
+    (connect a1 @me)
+    (connect a2 @me)
+    (connect sum @me)
+    dispatcher))
 
 (defn subtractor [a1 a2 sum]
-	(let [me (atom nil)
-		  dispatcher {
-			:type "Subtractor"
-			:i-have-a-value (fn []
-				(cond (and (has-value? a1) (has-value? a2))
-					    (set-value! sum (- (get-value a1) (get-value a2)) @me)
-					  (and (has-value? a1) (has-value? sum))
-					    (set-value! a2 (- (get-value a1) (get-value sum)) @me)
-					  (and (has-value? a2) (has-value? sum))
-					    (set-value! a1 (+ (get-value sum) (get-value a2)) @me)))
-			:i-lost-my-value (fn []
-				(forget-value! sum @me)
-				(forget-value! a1 @me)
-				(forget-value! a2 @me))
-		}]
-		(reset! me dispatcher)
-		(connect a1 @me)
-		(connect a2 @me)
-		(connect sum @me)
-		dispatcher))
+  (let [me (atom nil)
+        dispatcher {
+                    :type "Subtractor"
+                    :i-have-a-value
+                    (fn []
+                      (let [[[hv1 hv2 hvs][v1 v2 vs]] (dmap [has-value? get-value] [a1 a2 sum])]
+                        (cond
+                          (and hv1 hv2) (set-value! sum (- v1 v2) @me)
+                          (and hv1 hvs) (set-value! a2 (- a2 vs) @me)
+                          (and hv2 hvs) (set-value! a1 (+ vs v2) @me))))
+                    :i-lost-my-value
+                    (fn []
+                      (forget-value! sum @me)
+                      (forget-value! a1 @me)
+                      (forget-value! a2 @me))
+                    }]
+    (reset! me dispatcher)
+    (connect a1 @me)
+    (connect a2 @me)
+    (connect sum @me)
+    dispatcher))
 
 (defn multiplier [m1 m2 product]
-	(let [me (atom nil)
-		  dispatcher {
-			:type "Multiplier"
-			:i-have-a-value (fn []
-				(cond (or (and (has-value? m1) (= 0 (get-value m1)))
-					      (and (has-value? m2) (= 0 (get-value m2))))
-		                (set-value! product 0 @me)
-					  (and (has-value? m1) (has-value? m2))
-					    (set-value! product (* (get-value m1) (get-value m2)) @me)
-					  (and (has-value? m1) (has-value? product))
-					    (set-value! m2 (/ (get-value product) (get-value m1)) @me)
-					  (and (has-value? m2) (has-value? product))
-					    (set-value! m1 (/ (get-value product) (get-value m2)) @me)))
-			:i-lost-my-value (fn []
-				(forget-value! product @me)
-				(forget-value! m1 @me)
-				(forget-value! m2 @me))
-		}]
-		(reset! me dispatcher)
-		(connect m1 @me)
-		(connect m2 @me)
-		(connect product @me)
-		dispatcher))
+  (let [me (atom nil)
+        dispatcher {
+                    :type "Multiplier"
+                    :i-have-a-value
+                    (fn []
+                      (let [[[hv1 hv2 hvp][v1 v2 vp]] (dmap [has-value? get-value] [m1 m2 product])]
+                        (cond
+                          (or (and hv1 (= 0 v1)) (and hv2 (= 0 v2)))
+                          	(set-value! product 0 @me)
+                          (and hv1 hv2) (set-value! product (* v1 v2) @me)
+                          (and hv1 hvp) (set-value! m2 (/ vp v1) @me)
+                          (and hv2 hvp) (set-value! m1 (/ vp v2) @me))))
+                    :i-lost-my-value
+                    (fn []
+                      (forget-value! product @me)
+                      (forget-value! m1 @me)
+                      (forget-value! m2 @me))
+                    }]
+    (reset! me dispatcher)
+    (connect m1 @me)
+    (connect m2 @me)
+    (connect product @me)
+    dispatcher))
 
 (defn divider [d1 d2 result]
 	(let [me (atom nil)
@@ -79,6 +81,7 @@
 			:i-have-a-value (fn []
                      (let [[[hv1 hv2 hvr] [v1 v2 vr]] (dmap [has-value? get-value] [d1 d2 result])]
 						(cond (and hv1 (= 0 v1)) (set-value! result 0 @me)
+            				  (and hvr (= 0 vr)) (set-value! d1 0 @me)
 							  (and hv1 hv2) (set-value! result (/ v1 v2) @me)
 							  (and hv1 hvr) (set-value! d2 (/ v1 vr) @me)
 							  (and hv2 hvr) (set-value! d1 (* vr v2) @me))))
@@ -110,12 +113,12 @@
   (let [me (atom nil)
         dispatcher {
                     :type "Passthrough"
-                    :i-have-a-value (fn []
-                                      (let [[[hv1 hv2] [v1 v2]]
-                                            (dmap [has-value? get-value] [p1 p2])]
-                                        (cond
-                                          hv1 (set-value! p2 v1 @me)
-                                          hv2 (set-value! p1 v2 @me))))
+                    :i-have-a-value
+                    (fn []
+                      (let [[[hv1 hv2] [v1 v2]] (dmap [has-value? get-value] [p1 p2])]
+                        (cond
+                          hv1 (set-value! p2 v1 @me)
+                          hv2 (set-value! p1 v2 @me))))
                     :i-lost-my-value (fn []
                                        (forget-value! p1 @me)
                                        (forget-value! p2 @me))
--- test/constraints/elements_test.clj
+++ test/constraints/elements_test.clj
@@ -2,26 +2,57 @@
   (:use clojure.test
         constraints.interface constraints.elements))
 
+; utility-functions
+
+(defn get-connectors [num]
+  (let [get-connectors-impl
+        (fn [num-remaining items]
+          (if  (= 0 num-remaining)
+            items
+            (recur (- num-remaining 1) (cons (make-connector) items))))]
+  (get-connectors-impl num [])))
+    
+(defn work-ports [port1 port2]
+  (set-value! port1 100 :user)  
+  (forget-value! port1 :user)
+  
+  (set-value! port2 5 :user)  
+  (forget-value! port2 :user)
+  
+  :done)
+
+(defn do-3port-test [component]
+  (let [[p1 p2 result] (get-connectors 3)]
+    (component p1 p2 result)
+    
+    (probe "P1" p1)
+    (probe "P2" p2)
+    (probe "Result" result)
+    (constant 10 p2)
+    
+    (work-ports p1 result)))
+
+; self-tests
+
+(deftest get-connectors-test
+  (testing "Get connectors self-test"
+    (is (= 3
+        (count (get-connectors 3))))))
+
+; elements-not-crashing tests
+
+(deftest test-adder
+  (testing "Adder element"
+    (is (= :done (do-3port-test adder)))))
+
+(deftest test-subtractor
+  (testing "Subtractor element"
+    (is (= :done (do-3port-test subtractor)))))
+
+(deftest test-multiplier
+  (testing "Multiplier element"
+    (is (= :done (do-3port-test multiplier)))))
+
 (deftest test-divider
   (testing "Divider element"
-    (is (= :done
-           (do
-             (def d1 (make-connector))
-             (def d2 (make-connector))
-             (def result (make-connector))
-             
-             (divider d1 d2 result)
-             
-             (probe "D1" d1)
-             (probe "D2" d2)
-             (probe "Result" result)
-             
-             (constant 10 d2)
-             
-             (set-value! d1 100 :user)  
-             (forget-value! d1 :user)
-             
-             (set-value! result 5 :user)  
-             (forget-value! result :user)
-             
-             :done)))))
+    (is (= :done (do-3port-test divider)))))
